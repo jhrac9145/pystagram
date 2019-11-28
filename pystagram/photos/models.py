a8ea0e95f0f1489bc23d76e254dbf1545f599413
joohyeong <joohyenog2001@gmail.com>
2019-11-28 19:24:44 +0900

    blabla

---
+++ pystagram/photos/models.py
@@ -0,0 +1,23 @@
+
+from __future__ import unicode_literals
+
+from django.db import models
+# from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
+from django.conf import settings
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='%Y/%m/%d/orig')
+    filtered_image = models.ImageField(upload_to='%Y/%m/%d/filtered')
+    content = models.TextField(max_length=500, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        self.filtered_image.delete()
+        super(Photo, self).delete(*args, **kwargs)
+
+    def get_absolute_url(self):
+        url = reverse_lazy('detail', kwargs={'pk': self.pk})
+        return url
